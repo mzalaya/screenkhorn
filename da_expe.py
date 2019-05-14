7ed0b4fe1928b0f1afd95437ecbc7683bd8ce3cb
arakotom <Vous@exemple.com>
2019-05-14 10:32:34 +0200

    expe

--- da_expe.py
+++ da_expe.py
@@ -9,18 +9,32 @@ from time import process_time as time
 import matplotlib.pylab as plt
 import ot
 import ot.plot
-
 import  da_screenkhorn
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import argparse
 
 
-n_samples_source = 2000
-n_samples_target = 2000
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', action='store', dest='n', default = 1000, type=int,
+                        help='number of samples ')
+parser.add_argument('-d', action='store', dest='d', default = 1, type=int,
+                        help='dataset type ')
+arguments = parser.parse_args()
+n = arguments.n # number of samples per class
+
+if arguments.d == 1:
+    data = 'toy'
+else:
+    data = 'mnist'
+    
+
+n_samples_source = n
+n_samples_target = n
 
 nb_iter = 50
 pathres='./resultat/'
-p_vec = [2,10,20,30]
+p_vec = [1.5,2,5,10]
 n_pvec=  len(p_vec)
 
 bc_sink = np.zeros(nb_iter)
@@ -31,11 +45,11 @@ for i in range(nb_iter):
 
     print('iter:',i)
 
-    Xs, ys = ot.datasets.make_data_classif('3gauss', n_samples_source,nz=0.5,random_state=i)
-    Xt, yt = ot.datasets.make_data_classif('3gauss2', n_samples_target,nz=0.5, random_state=i)
+    Xs, ys = ot.datasets.make_data_classif('3gauss', n_samples_source,nz=0.75,random_state=i)
+    Xt, yt = ot.datasets.make_data_classif('3gauss2', n_samples_target,nz=0.75, random_state=i)
     M = ot.dist(Xs, Xt, metric='sqeuclidean')
     
-    filename = 'da'
+    filename = 'da_{:}_{:d}'.format(data,n)
     #%% 
     # Sinkhorn Transport
     tic = time()
@@ -71,6 +85,9 @@ for i in range(nb_iter):
              bc_sink= bc_sink,  bc_screen= bc_screen,
              time_sink = time_sink,  time_screen = time_screen)
     
-    
-print('gain : {:2.2f}'.format(time_sink.mean()/time_screen.mean(axis=0)))
+#%%
+mean_screen_time = time_screen.mean(axis=0)
+for i,p in enumerate(p_vec):
+    print('gain dec {:1.2f}: {:2.2f}'.format(p,time_sink.mean()/mean_screen_time[i]))
+        
 print('perf sink : {:2.2f}, perf screen {:2.2f}'.format(bc_sink.mean(),bc_screen.mean()))
